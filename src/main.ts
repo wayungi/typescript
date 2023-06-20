7b9ef7b53aec1ab06c3d6dc4df5b0f7cc992f7ba
wayungi <fwayungi@gmail.com>
2023-06-20 20:29:07 +0300

    ADD: learner notes

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,10 @@ console.log(username)
 
 // tsc filename.ts -w :will watch & recompile 
 // tsc --init : create the typescript config file
+// tsc -w :watch the src folder and compile all typescript files. this is only possible afer initializing typescript with the !tsc --init command
+
+/* add this in the main tsconfig object to exclude all .ts files that are not inside the src folder from being compile
+"include" : [
+    "src"
+  ]
+  */
